0f386ba8ca56a53a7f442c26f8edf50cee193af9
Andreas <andiburger@gmail.com>
2026-01-31 17:08:55 +0100

    added battery info

--- growatt.py
+++ growatt.py
@@ -567,10 +567,11 @@ class Growatt:
             block1 = self._read_block(3000, 125, MAP_MOD_TL3_XH, is_input_reg=True)
             if block1:
                 data.update(block1)
-            # Block 2: Battery/BDC Data (3125-3249)
-            #block2 = self._read_block(3125, 125, MAP_MOD_TL3_XH, is_input_reg=True)
-            #if block2:
-            #    data.update(block2)
+            block2 = self._read_block(3125, 125, MAP_MOD_TL3_XH, is_input_reg=True)
+            if block2:
+                for key, value in block2.items():
+                    if key not in data or (value != 0 and value != 0.0):
+                        data[key] = value
         else:
             self.log.warning(f"No valid register map found for model: {self.model}")
             self.log.warning(self.get_supported_models_help)
